17838364c3be24836830b3984e63998500ca5d27
Kurt <kgoedgebuer-byte@users.noreply.github.com>
2025-12-03 12:49:17 +0100

    Fix: iconen opnieuw (Pillow textbbox) + PWA/iOS links + SW v10

--- docs/budget/sw.js
+++ docs/budget/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kgb-budget-v6';
+const CACHE = 'kgb-budget-v10';
 self.addEventListener('install', e => e.waitUntil(
   caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.webmanifest']))
 ));
--- docs/full/sw.js
+++ docs/full/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kgb-full-v6';
+const CACHE = 'kgb-full-v10';
 self.addEventListener('install', e => e.waitUntil(
   caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.webmanifest']))
 ));
